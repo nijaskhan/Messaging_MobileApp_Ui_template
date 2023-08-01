b3756b4ab85ce0ca3e911ecb13d993b3d63cc1b2
nijaskhan <nijaskhan93@gmail.com>
2023-08-01 10:32:47 +0530

    git initialized

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:list_app/screen_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green.shade200
+      ),
+      home: ScreenHome(),
+    );
+  }
+}
---
+++ lib/screen_home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+
+class ScreenHome extends StatelessWidget {
+  const ScreenHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List view sample project"),
+        shadowColor: Colors.blueGrey,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 14, right: 5),
+          child: Image.network('https://external-content.duckduckgo.com/iu/?u='
+              'https%3A%2F%2Flogodownload.org%2Fwp-content%2Fuploads%2F2015%2F04%2Fwhatsapp-logo-1.'
+              'png&f=1&nofb=1&ipt=87090c317ac540403ce76166a556207a92a747f6380efa6e5ffd63455ad45f00&ipo=images'),
+        ),
+      ),
+      body: SafeArea(
+          child: ListView.separated(
+        itemBuilder: (ctx, index){
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: NetworkImage('https://cdn.iconscout.com/icon/free/png-512/avatar-370-456322.png'),
+            ),
+            title: Text("nijas"),
+            subtitle: Text("one message has arrived"),
+            trailing: Column(
+              children: [
+                badges.Badge(
+                  badgeContent: Text("${index+1}", style: TextStyle(color: Colors.white)),
+                  badgeColor: Colors.green.shade500,
+                ),
+                Spacer(),
+                Text("$index : 30 pm"),
+              ],
+            )
+          );
+        },
+        separatorBuilder: (ctx, index){
+          return Divider();
+        },
+        itemCount: 20,
+      )),
+    );
+  }
+}
