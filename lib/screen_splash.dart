529ea66e7701f87262ce79ed22f89e474d540830
nijaskhan <nijaskhan93@gmail.com>
2023-08-02 16:01:38 +0530

    logo updated

--- lib/screen_splash.dart
+++ lib/screen_splash.dart
@@ -20,8 +20,7 @@ class _ScreenSplashState extends State<ScreenSplash> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Image.network('https://logolook.net/wp-content/uploads/'
-              '2021/06/Whatsapp-Logo.png', height: 100),
+          child: Image.asset('assets/Whatsapp-Logo.png', height: 100),
         ),
       ),
     );
